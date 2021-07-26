8fa3a7c99c1128355d49a483eaabe1ae688535f7
tenzin woeser <tenzinwoeser15@gmail.com>
2021-07-26 09:25:16 +0530

    Redux setup done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
-  const a = "";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with eslint
-        </a>
-      </header>
+      <Provider store={store}></Provider>
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducer";
+const middleWare = [thunk];
+
+const initialState = {};
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleWare))
+);
+export default store;
